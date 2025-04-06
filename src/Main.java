63b92015d20a6e52e9604b91c2e4979aaff89dfd
Thony1124 <jpazc2425@politecnics.barcelona>
2025-04-06 14:50:18 +0200

    Modificaciones Array commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -6,6 +8,14 @@ public class Main {
         System.out.println("que pasa");
 
         System.out.println("nuevo commit");
+        ArrayList<String> numeros = new ArrayList<>();
+        numeros.add("1");
+        numeros.add("2");
+        numeros.add("3");
+
+        for (String numero : numeros) {
+            System.out.println(numero);
+        }
 
     }
 }
